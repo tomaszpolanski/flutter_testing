08d6f9e31e16606f1ad3afb6829f27b518d2dc15
Tomek Polanski <tpolanski@groupon.com>
2018-12-04 07:04:02 +0100

    Update unit_test.dart

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,6 +2,14 @@ import 'package:flutter_testing/http.dart';
 import 'package:http/http.dart' show Response, get;
 import 'package:test_api/test_api.dart';
 
+const userResponse = '''
+{
+  "name": "John",
+  "avatar_url": "http://some.image",
+  "repos_url": "http://some.repo"
+}
+''';
+
 void main() {
   group('Http client', () {
     test('fetches users', () async {
@@ -36,11 +44,3 @@ void main() {
     });
   });
 }
-
-const userResponse = '''
-{
-  "name": "John",
-  "avatar_url": "http://some.image",
-  "repos_url": "http://some.repo"
-}
-''';
